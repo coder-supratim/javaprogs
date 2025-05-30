be4f692fbf4ba969479ef8c5450d8e55241d0d58
coder-supratim <supratim.arya@gmail.com>
2025-05-29 22:02:31 -0400

    Basic thread refresher

---
+++ java.concepts/src/tim/concepts/algo/RunnableTask.java
@@ -0,0 +1,18 @@
+package tim.concepts.algo;
+
+public class RunnableTask implements Runnable {
+    private final long countUntil;
+
+    RunnableTask(long countUntil) {
+        this.countUntil = countUntil;
+    }
+
+    @Override
+    public void run() {
+        long sum = 0;
+        for (long i = 1; i < countUntil; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
